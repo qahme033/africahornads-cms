1575e24b7947f73cbddf1f500e3a72aeb8a371bb
Qasim <qahme033@gmail.com>
2022-11-26 00:48:29 -0500

    change callback redirect route

--- webio-callback-catch/src/index.ts
+++ webio-callback-catch/src/index.ts
@@ -2,7 +2,7 @@ import { defineEndpoint } from '@directus/extensions-sdk';
 import { Router, Request, Response} from 'express';
 
 export default defineEndpoint((router: Router) => {
-	router.get('/oauth/login', (_req: Request, res: Response) => {
+	router.get('/login/oauth/authorize', (_req: Request, res: Response) => {
 		// const url = process.env.PUBLIC_URL
 		res.redirect('/auth/login/github/callback');
 })});
